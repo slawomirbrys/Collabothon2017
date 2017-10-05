033186e9b953c62dab3196f6bf11a63ef23ed263
Sławomir Bryś <slawomir.brys+github@gmail.com>
2017-10-06 01:56:42 +0200

    Integration with bot

--- WebApplication/WebApplication/Controllers/HomeController.cs
+++ WebApplication/WebApplication/Controllers/HomeController.cs
@@ -31,5 +31,10 @@ namespace WebApplication.Controllers
         {
             return View();
         }
+
+        public ActionResult Bot()
+        {
+            return View();
+        }
     }
 }
