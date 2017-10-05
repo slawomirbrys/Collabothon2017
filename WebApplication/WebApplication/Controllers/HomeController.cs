3a7344b8d434d2ffccf1853ec2ac19f13e9e9976
Sławomir Bryś <slawomir.brys@gmail.com>
2017-10-05 16:03:00 +0200

    STT implementation

--- WebApplication/WebApplication/Controllers/HomeController.cs
+++ WebApplication/WebApplication/Controllers/HomeController.cs
@@ -26,5 +26,10 @@ namespace WebApplication.Controllers
 
             return View();
         }
+
+        public ActionResult Microphone()
+        {
+            return View();
+        }
     }
 }
